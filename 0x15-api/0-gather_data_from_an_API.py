a6dde5e5a7ee95e10b91f9a8688f83cf97c47253
chinedu06 <godswillchinedu644@gmail.com>
2024-07-29 16:46:14 -0700

    first commit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This script uses a REST API to return information about an employee's TODO list progress.
+"""
+import requests
+import sys
+
+def get_employee_todo_progress(employee_id):
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
+
+    if user_response.status_code != 200 or todos_response.status_code != 200:
+        print("Error fetching data")
+        return
+
+    user = user_response.json()
+    todos = todos_response.json()
+
+    employee_name = user.get("name")
+    total_tasks = len(todos)
+    done_tasks = [todo for todo in todos if todo.get("completed")]
+    num_done_tasks = len(done_tasks)
+
+    print(f"Employee {employee_name} is done with tasks({num_done_tasks}/{total_tasks}):")
+    for task in done_tasks:
+        print(f"\t {task.get('title')}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+    
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer")
+        sys.exit(1)
+
+    get_employee_todo_progress(employee_id)
+
---
+++ 0x15-api/README.md
@@ -0,0 +1,21 @@
+# 0x15. API
+
+## Description
+
+This project involves gathering data from a REST API and displaying an employee's TODO list progress.
+
+## Requirements
+
+- Allowed editors: vi, vim, emacs
+- All files will be interpreted/compiled on Ubuntu 20.04 LTS using python3 (version 3.8.5)
+- All files should end with a new line
+- The first line of all files should be exactly `#!/usr/bin/python3`
+- Libraries imported in Python files must be organized in alphabetical order
+- A `README.md` file, at the root of the folder of the project, is mandatory
+- Code should use the pycodestyle (version 2.8.*)
+- All files must be executable
+- The length of files will be tested using `wc`
+- All modules should have documentation
+- Dictionary values should be accessed using `get`
+- Code should not be executed when imported (by using `if __name__ == "__main__":`)
+
