7bf66f08f179890edd2c0c38e2ee90a5cb9111b5
chinedu06 <godswillchinedu644@gmail.com>
2024-07-29 16:49:04 -0700

    first commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,6 +5,7 @@ This script uses a REST API to return information about an employee's TODO list 
 import requests
 import sys
 
+
 def get_employee_todo_progress(employee_id):
     user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
@@ -28,11 +29,12 @@ def get_employee_todo_progress(employee_id):
     for task in done_tasks:
         print(f"\t {task.get('title')}")
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
-    
+
     try:
         employee_id = int(sys.argv[1])
     except ValueError:
