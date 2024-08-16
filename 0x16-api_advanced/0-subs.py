d72ce10d8fd257e17c45dd4e4baa0f711482502f
chinedu06 <godswillchinedu644@gmail.com>
2024-08-16 01:46:19 +0000

    first commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module to query the Reddit API for the number of subscribers of a subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers for a given subreddit.
+    If the subreddit is invalid, returns 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Python/requests'}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            return 0
+        
+        data = response.json().get('data', {})
+        return data.get('subscribers', 0)
+    
+    except requests.exceptions.RequestException:
+        return 0
+
