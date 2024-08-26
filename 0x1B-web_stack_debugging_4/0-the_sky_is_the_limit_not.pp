862273bf2d91fcc397e91f0ce3b5073663e64119
chinedu06 <godswillchinedu644@gmail.com>
2024-08-26 12:05:05 +0000

    first commit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,12 @@
-# This Puppet manifest is intended to optimize the Nginx server for handling high traffic by adjusting worker processes and connections.
+# Optimizes the Nginx server for handling high traffic by adjusting worker processes, connections, and other performance-related settings.
 
 exec { 'fix-nginx-performance':
   command => "/etc/init.d/nginx stop && \
     sed -i 's/worker_connections 768;/worker_connections 4096;/g' /etc/nginx/nginx.conf && \
     sed -i 's/worker_processes auto;/worker_processes 4;/g' /etc/nginx/nginx.conf && \
+    sed -i 's/keepalive_timeout 65;/keepalive_timeout 15;/g' /etc/nginx/nginx.conf && \
+    sed -i 's/#tcp_nopush on;/tcp_nopush on;/g' /etc/nginx/nginx.conf && \
+    sed -i 's/#tcp_nodelay on;/tcp_nodelay on;/g' /etc/nginx/nginx.conf && \
     /etc/init.d/nginx start",
   path    => '/usr/bin:/usr/sbin:/bin:/sbin',
 }
